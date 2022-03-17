74b8bbaeb22214ff6d139cdbf6a35a05e4129dc6
Ashutosh D. Kedar <ashutoshdkedar@gmail.com>
2022-03-17 18:26:59 +0530

    Deploying changes related Frontend required enhancements in project structure.

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgxSpinnerModule} from "ngx-spinner";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgxSpinnerModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
