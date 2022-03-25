7ab2b56f1c4da1a163c220b9f9ddc812d6380bce
nitin <nitinsgodase@gmail.com>
2022-03-25 13:04:56 +0530

    commit for dropdown value

---
+++ Backend/Inventory.Application.Shared/Dropdowns/Dropdown.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Application.Shared.Dropdowns
+{
+    public class Dropdown
+    {
+        public Guid Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
