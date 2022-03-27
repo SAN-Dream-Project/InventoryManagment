112b4b3164c6685618d5334b195b4e32af7e18df
nitin <nitinsgodase@gmail.com>
2022-03-27 10:53:42 +0530

    commit for purches get all api

--- Backend/Inventory.Application.Shared/Purchases/Dto/PurchaseDto.cs
+++ Backend/Inventory.Application.Shared/Purchases/Dto/PurchaseDto.cs
@@ -9,14 +9,14 @@ namespace Inventory.Application.Shared.Purchases.Dto
     public class PurchaseDto
     {
         public Guid Id { get; set; }
-        public Guid GoodID { get; set; }
-        public Guid GoodSupplierID { get; set; }
+        public string GoodName { get; set; }
+        public string GoodSupplierName { get; set; }
         public double? GrossGoodQuantity { get; set; }
         public double? GoodRate { get; set; }
-        public Guid? KadataID { get; set; }
+        public string KadataQuantity { get; set; }
         public int? KadtaTotal { get; set; }
         public double? NetGoodQuantity { get; set; }
-        public Guid? LabourRateID { get; set; }
+        public double LabourRate { get; set; }
         public double? TotalLabourCosting { get; set; }
         public double? TotalAmout { get; set; }
         public string? CreatedBy { get; set; }
