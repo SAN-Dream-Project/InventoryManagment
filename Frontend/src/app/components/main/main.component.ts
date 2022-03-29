b5713302fe0833c3c4a54a2bf51e9d50df814ba8
Ashutosh D. Kedar <ashutoshdkedar@gmail.com>
2022-03-29 18:59:45 +0530

    Deploying changes related to Frontend.

--- Frontend/src/app/components/main/main.component.ts
+++ Frontend/src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgxSpinnerService} from "ngx-spinner";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ngxSpinnerService: NgxSpinnerService) { }
 
   ngOnInit(): void {
+    this.ngxSpinnerService.show();
+    setTimeout(() => {
+      this.ngxSpinnerService.hide();
+    }, 1000);
   }
 
 }
